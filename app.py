8a998649791b75fcad4fa1cf2223c4c1fa4de4bb
GreatBrittan <clbr8512@colroado.edu>
2023-11-07 22:57:46 +0000

    Push 6
    Minor changes and bug fixes

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def selection():
         respone_string+="<tr>"
         for info in player:
             response_string+="<td>{}</td>".format(info)
-        response_string+="<tr>"
+        response_string+="</tr>"
     response_string+="</table>"
     return response_string
 @app.route('/db_route')
