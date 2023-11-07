8765fc830c8a70a80887113d361569608312f202
GreatBrittan <clbr8512@colroado.edu>
2023-11-07 22:31:28 +0000

    Push 2
    App.route('/db_create') has been created

--- app.py
+++ app.py
@@ -12,3 +12,19 @@ def testing():
     conn = psycopg2.connect('postgres://lab10webserivce_db_user:SsT2IplvuawEg78StJeXKuI3VFBwrs9B@dpg-cl5al8a8vr0c73ajrrqg-a/lab10webserivce_db')
     conn.close()
     return "Database Connection Successful"
+@app.route('/db_creat')
+def creation():
+    conn = psycopg2.connect('postgres://lab10webserivce_db_user:SsT2IplvuawEg78StJeXKuI3VFBwrs9B@dpg-cl5al8a8vr0c73ajrrqg-a/lab10webserivce_db')
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Created"
